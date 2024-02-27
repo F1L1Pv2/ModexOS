3a02d228b3d3b4598771c52fdb278835729407f0
Rilax <110360915+RilaxYT@users.noreply.github.com>
2024-02-27 19:07:13 +0100

    LOVE!!!

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -9,7 +9,7 @@
 
 global_color: db 0x0a
 welcome_msg:  db "Made by: F1L1P and Rilax",10,10
-              db "Modex 32 bit prot-",compile_day,compile_month,compile_year," (proteted mode edition)",10
+              db "System Modex 32 bit prot-",compile_day,compile_month,compile_year," (proteted mode edition)",10
               db "Copyright (C) 2020-",compile_year,"r.",10,10,0
 terminal_msg: db "#> ",0
 
@@ -164,7 +164,7 @@ run_command:
 
     .love_command:
     ; call cls
-    ; call valentine
+    call valentine
     call new_line
     jmp .after
 .after:
