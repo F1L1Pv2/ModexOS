9cd6ccc6d123b1a754c20c4ff1fab2e741e593ba
F1L1P <mlodzik.filip@wp.pl>
2024-03-20 21:28:09 +0100

    changed

--- src/bootloader/boot_io.asm
+++ src/bootloader/boot_io.asm
@@ -152,67 +152,67 @@ ScreenBuffer  equ 0xB8000
 
 ;     ret
 
-binary_hexadecimal:
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ;; funkcja binary to decimal (16-bits) ;;
-    ;;           AX to wejsce              ;;
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; binary_hexadecimal:
+;     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;     ;; funkcja binary to decimal (16-bits) ;;
+;     ;;           AX to wejsce              ;;
+;     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    push edx
-    push ecx
-    push ebx
-    push eax
+;     push edx
+;     push ecx
+;     push ebx
+;     push eax
 
-    xor ebx, ebx
+;     xor ebx, ebx
 
-    .loop:
-        xor edx, edx
-        mov ecx, 16
-        div ecx
-        mov ecx, eax
+;     .loop:
+;         xor edx, edx
+;         mov ecx, 16
+;         div ecx
+;         mov ecx, eax
 
-        mov al, dl
-        cmp al, 10
-        jge .letter
-    .number:
-        add al, '0'
-        jmp .after
-    .letter:
-        add al, 'A' - 10
-    .after:
-        push eax
+;         mov al, dl
+;         cmp al, 10
+;         jge .letter
+;     .number:
+;         add al, '0'
+;         jmp .after
+;     .letter:
+;         add al, 'A' - 10
+;     .after:
+;         push eax
 
-        mov eax, ecx
+;         mov eax, ecx
 
-        inc ebx
+;         inc ebx
 
-        test ecx, ecx
-        jnz .loop
+;         test ecx, ecx
+;         jnz .loop
 
-        mov ah, GLOBAL_COLOR
-        mov al, '0'
-        call write_char
-        inc word [cursor]
-        mov al, 'x'
-        call write_char
-        inc word [cursor]
+;         mov ah, GLOBAL_COLOR
+;         mov al, '0'
+;         call write_char
+;         inc word [cursor]
+;         mov al, 'x'
+;         call write_char
+;         inc word [cursor]
 
-    .flip:
-        pop eax
-        mov ah, GLOBAL_COLOR
-        call write_char
-        inc word [cursor]
+;     .flip:
+;         pop eax
+;         mov ah, GLOBAL_COLOR
+;         call write_char
+;         inc word [cursor]
 
-        dec ebx
-        jnz .flip
+;         dec ebx
+;         jnz .flip
 
-        pop eax
-        pop ebx
-        pop ecx
-        pop edx
+;         pop eax
+;         pop ebx
+;         pop ecx
+;         pop edx
 
-        call update_cursor
-        ret
+;         call update_cursor
+;         ret
 
 new_line:
     ;;F1L1P;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
